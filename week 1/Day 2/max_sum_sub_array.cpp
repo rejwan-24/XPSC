05fc7491dd0a0c841592e9f89af06b083d1aa221
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-03-16 23:19:57 +0600

    Update max_sum_sub_array.cpp

---
+++ week 1/Day 2/max_sum_sub_array.cpp
@@ -0,0 +1,23 @@
+class Solution{   
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr , int N){
+        int l = 0 , r = 0;
+        long long sum = 0, ans = 0;
+        while(r < N)
+        {
+            sum += Arr[r];
+            if((r - l + 1) == K)
+            {
+                ans = max(ans,sum);
+                sum -= Arr[l];
+                l++;
+                r++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        return ans;
+    }
+};
