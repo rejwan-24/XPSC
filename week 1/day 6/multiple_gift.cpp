d7a41bbd61078cb97064faee7a7f30d4d0258af5
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-03-20 23:27:08 +0600

    Update multiple_gift.cpp

--- week 1/day 6/multiple_gift.cpp
+++ week 1/day 6/multiple_gift.cpp
@@ -6,17 +6,16 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
  
-    ll a, b;
-    cin >> a >> b;
-
-    ll x = a;
+    ll x,y;
+    cin>>x>>y;
     ll cnt = 0;
-    while (x <= b)
+
+    while (x <= y)
     {
-        // cout << x << " ";
         cnt++;
         x *= 2;
     }
-    cout << cnt << endl;
+    
+    cout<<cnt<<endl;
     return 0;
 }
