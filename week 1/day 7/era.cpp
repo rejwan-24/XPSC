929d28437d2632f6005a855db0a047664bff79dc
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-03-21 22:56:36 +0600

    Update era.cpp

--- week 1/day 7/era.cpp
+++ week 1/day 7/era.cpp
@@ -28,35 +28,3 @@ int main()
     }
     return 0;
 }
-/*
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-
-    while (t--) {
-        int n;
-        cin >> n;
-
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
-        }
-
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int r = a[i] - (i + 1);
-            if (r > ans) {
-                ans = r;
-            }
-        }
-
-        cout << ans << endl;
-    }
-
-    return 0;
-}
-
-*/
