abc67c2066f30ba2b04bebca36c17da1c62f28cb
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-04-25 23:49:20 +0600

    Update Xorry_1.cpp

--- week 4/day 1/day 5/Xorry_1.cpp
+++ week 4/day 1/day 5/Xorry_1.cpp
@@ -13,21 +13,5 @@ int main()
     }
     return 0;
 }
-/*
-#include <bits/stdc++.h>
-using namespace std;
-int main() 
-{
-	int t;
-	cin>>t;
-	while(t--)
-    {
-	    int x;
-	    cin>>x;   
-	    cout<<(x^__bit_floor(x))<<" "<<__bit_floor(x)<<endl;
-        //__bit_floor() -> finds the largest integral power of two not greater than the given value 
-	}
-}
 
-*/
 
