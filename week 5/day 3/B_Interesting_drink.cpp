970d6594cf0fe98fbce9e4f6e3f54fc7eaed3015
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-05-06 22:11:57 +0600

    Update B_Interesting_drink.cpp

--- week 5/day 3/B_Interesting_drink.cpp
+++ week 5/day 3/B_Interesting_drink.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
-           
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int val;
+        cin>>val;
+ 
+        //use lower bound & upper bound concept of binary search
+    
+        int ans= upper_bound(v.begin(),v.end(),val) - v.begin();
+    
+        cout<<ans<<endl;
+    }
     return 0;
 }
