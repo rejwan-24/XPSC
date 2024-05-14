eb681d9241c5fde02526ffa53183a421d7eb03f9
rejwan-24 <163105739+rejwan-24@users.noreply.github.com>
2024-05-14 23:34:55 +0600

    Update B_Worms.cpp

--- week 5/day 7/B_Worms.cpp
+++ week 5/day 7/B_Worms.cpp
@@ -5,23 +5,22 @@ int main()
 {
     int n;
     cin>>n;
-	vector<int> v(n);
-	for(int i=0;i<n;i++)
+    vector<int> v(n);
+    for(int i=0;i<n;i++)
     {
-		cin>>v[i];
-		if(i != 0)
+	cin>>v[i];
+	if(i != 0)
         {
-			v[i]+=v[i-1];
-		}
+	      v[i]+=v[i-1];
 	}
-	int m;cin>>m;
-	while(m--)
+    }
+    int m;cin>>m;
+    while(m--)
     {
-		int x;
+	int x;
         cin>>x;
-		int val=lower_bound(v.begin(),v.end(),x)-v.begin();
-		cout<<val+1<<endl;
-	}
-	
+	int val=lower_bound(v.begin(),v.end(),x)-v.begin();
+	cout<<val+1<<endl;
+     }	
     return 0;
 }
